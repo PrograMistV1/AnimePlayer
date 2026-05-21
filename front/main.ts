24386787d3d6e7bc16497960d6ffb42cb6cf9e8f
PrograMistV1 <programist.v.1.1@gmail.com>
2026-05-21 14:44:31 +0200

    refactor: update layout and styles for improved user experience and accessibility

--- front/main.ts
+++ front/main.ts
@@ -1,13 +1,12 @@
 import {loadAnimeData} from "./api/animeApi.ts";
 import {setAnimeData} from "./state/playerState.ts";
-import {initTheme} from "./components/theme.ts";
 import {initSearch} from "./components/search.ts";
 import {initContinueWatching} from "./components/continueWatching.ts";
 import {initPlayer} from "./components/player.ts";
 import "./components/videoPlayer.ts";
 
 document.addEventListener("DOMContentLoaded", async () => {
-    initTheme();
+    //initTheme(); todo вернуть темы
     initSearch();
     initPlayer();
 
